a7ded9b3de8b1550ae8a563e913e21c39f1cb81b
mdhar6 <mdhar6@c02yl2jqjgh6.uk.aexp.com>
2019-09-23 16:24:29 +0100

    Set up code

---
+++ src/main/java/training/busboard/Client.java
@@ -0,0 +1,8 @@
+package training.busboard;
+
+public class Client {
+    String naptanID;   //this is the stop code//
+    String route;
+    String destination;
+    Integer timeToStation;
+}
--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -1,7 +1,33 @@
 package training.busboard;
 
+import org.glassfish.jersey.jackson.JacksonFeature;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String args[]) {
-        // Your code here!
+
+    private static void createUserInput(String APIString) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter a stop code below: ");
+        String userInput = in.nextLine();
+        APIString += userInput +"/Arrivals";
+        System.out.println(APIString);
     }
+
+    public static void main(String args[]) {
+
+//        Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
+//        String name = client.target("https://api.tfl.gov.uk/StopPoint/490008660N").request(MediaType.TEXT_PLAIN).get(String.class);
+        String APIString = "https://api.tfl.gov.uk/StopPoint/";
+
+        createUserInput(APIString);
+
+    }
+
 }	
