bb853a46ef35561c25014c505e331e5b2813dd17
mdhar6 <mdhar6@c02yl2jqjgh6.uk.aexp.com>
2019-09-26 10:21:14 +0100

    Set up complete

--- src/main/java/training/busboard/Client.java
+++ src/main/java/training/busboard/Client.java
@@ -1,8 +1,16 @@
 package training.busboard;
 
 public class Client {
-    String naptanID;   //this is the stop code//
+    String naptanID;   //this is the stopCode
     String route;
     String destination;
     Integer timeToStation;
+
+    Client(String naptanID, String route, String destination, int timeToStation) {
+        this.naptanID = naptanID;
+        this.route = route;
+        this.destination = destination;
+        this.timeToStation = timeToStation;
+    }
+
 }
--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -2,32 +2,37 @@ package training.busboard;
 
 import org.glassfish.jersey.jackson.JacksonFeature;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Invocation;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.util.Scanner;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 
 public class Main {
 
-    private static void createUserInput(String APIString) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Please enter a stop code below: ");
-        String userInput = in.nextLine();
-        APIString += userInput +"/Arrivals";
-        System.out.println(APIString);
+    public static void main(String args[]) throws KeyManagementException, NoSuchAlgorithmException {
+
+        System.setProperty("http.proxyHost", "localhost");
+        System.setProperty("http.proxyPort", "9090");
+        System.setProperty("https.proxyHost", "localhost");
+        System.setProperty("https.proxyPort", "9090");
+
+        SSLContext sslcontext = SSLContext.getInstance("TLS");
+
+        sslcontext.init(null, new TrustManager[]{new X509TrustManager() {
+            public void checkClientTrusted(X509Certificate[] arg0, String arg1) {}
+            public void checkServerTrusted(X509Certificate[] arg0, String arg1) {}
+            public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
+        }}, new java.security.SecureRandom());
+
+        Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).sslContext(sslcontext).hostnameVerifier((s1, s2) -> true).build();
+        String response = client.target("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals")
+                .request("text/json")
+                .get(String.class);
+
+        System.out.println(response);
     }
-
-    public static void main(String args[]) {
-
-//        Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
-//        String name = client.target("https://api.tfl.gov.uk/StopPoint/490008660N").request(MediaType.TEXT_PLAIN).get(String.class);
-        String APIString = "https://api.tfl.gov.uk/StopPoint/";
-
-        createUserInput(APIString);
-
-    }
-
-}	
+}
